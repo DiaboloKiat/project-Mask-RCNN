82ded329943a43089b3ca6af39b8be08bfa1f57b
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-22 23:01:19 +0800

    06_22 on detectron2

--- git-pull.sh
+++ git-pull.sh
@@ -18,18 +18,18 @@ if [ "$CONFLICTS" -gt 0 ] ; then
    return 1
 fi
 
-echo "-----------------------------------------------------------------------"
-echo "-------------------------pull detectron2-------------------------------"
-echo "-----------------------------------------------------------------------"
-cd ~/project-Mask-RCNN/catkin_ws/src/mask_rcnn
-git checkout $BRANCH
-git pull
+# echo "-----------------------------------------------------------------------"
+# echo "-------------------------pull detectron2-------------------------------"
+# echo "-----------------------------------------------------------------------"
+# cd ~/project-Mask-RCNN/catkin_ws/src/mask_rcnn
+# git checkout $BRANCH
+# git pull
 
-CONFLICTS=$(git ls-files -u | wc -l)
-if [ "$CONFLICTS" -gt 0 ] ; then
-   echo "There is conflict in detectron2. Aborting"
-   return 1
-fi
+# CONFLICTS=$(git ls-files -u | wc -l)
+# if [ "$CONFLICTS" -gt 0 ] ; then
+#    echo "There is conflict in detectron2. Aborting"
+#    return 1
+# fi
 
 cd ~/project-Mask-RCNN
 return 0
